35e8c566b9ee7681afcc04a7d3d40f34eb38a3b1
Noureddine AMRANE <amranenoureddin3@gmail.com>
2019-02-13 15:05:46 +0100

    question 6

--- src/TP3/RepeatNetwork.java
+++ src/TP3/RepeatNetwork.java
@@ -3,6 +3,8 @@ package TP3;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
 
@@ -10,11 +12,13 @@ public class RepeatNetwork implements Runnable{
     private SocketChannel socketChannel;
     private ByteBuffer byteBuffer;
     private Client client;
+    private ReadableByteChannel readableByteChannel;
 
     public RepeatNetwork(SocketChannel socketChannel, Client client) {
         this.socketChannel = socketChannel;
         this.client = client;
         this.byteBuffer = ByteBuffer.allocate(512);
+        this.readableByteChannel = Channels.newChannel(System.in);
     }
 
     @Override
