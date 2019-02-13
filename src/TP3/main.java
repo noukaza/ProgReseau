22885a7de08e1168ed4afa6822330b23289f9e26
Noureddine AMRANE <amranenoureddin3@gmail.com>
2019-02-13 15:24:30 +0100

    clean code

--- src/TP3/main.java
+++ src/TP3/main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class main {
     public static void main(String[] args) throws IOException {
-        Client client = new Client("access759336177.webspace-data.io",22);
+        new Client("access759336177.webspace-data.io",22);
     }
 }
 
